e7aed83a4959cf97b3db25ca8d52fa948f1f24ce
george91731 <103102617+george91731@users.noreply.github.com>
2024-11-01 16:52:02 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -54,43 +54,55 @@ def read_busy_bit(bus, address):
         return False
 
 
-def program_flash(bus, address, data):
-    addr_bytes = address.to_bytes(4, 'big')
-    data_bytes = data.to_bytes(4, 'big')    
-    packets = addr_bytes + data_bytes
-    print(f'Programming address {address:08X}: data {data:08X}')     
-    print(f'Sending packets: {" ".join(f"{byte:02X}" for byte in packets)}')
-    msg = i2c_msg.write(MAX10_I2C_SLAVE_ADDRESS, packets)
-    bus.i2c_rdwr(msg)
-
+def program_flash(bus, addr, data):
+    # addr_bytes = address.to_bytes(4, 'big')
+    # data_bytes = data.to_bytes(4, 'big')    
+    # packets = addr_bytes + data_bytes
+    # print(f'Programming address {address:08X}: data {data:08X}')     
+    # print(f'Sending packets: {" ".join(f"{byte:02X}" for byte in packets)}')
+    # msg = i2c_msg.write(MAX10_I2C_SLAVE_ADDRESS, packets)
+    # bus.i2c_rdwr(msg)
+    try:
+        outbuf = bytearray(8)
+        outbuf[0] = (addr >> 24) & 0xFF
+        outbuf[1] = (addr >> 16) & 0xFF
+        outbuf[2] = (addr >> 8) & 0xFF
+        outbuf[3] = addr & 0xFF
+        outbuf[4:8] = data
+        msg = i2c_msg.write(MAX10_I2C_SLAVE_ADDRESS, outbuf)
+        bus.i2c_rdwr(msg)
+    except Exception as e:
+            print(f"Error: {e} ")
 def program_flash_from_file(bus, file_path, START_ADDR, END_ADDR):
     addr = START_ADDR
-    addr_increment = 4
 
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            hex_data = line.strip().split()
+    with open(file_path, 'r') as f: 
+        for line in f:                   
+            
+            hex_data_part = line[5:54].strip()  
+            hex_data = hex_data_part.split()          
+            print(f'Extracted hex data: {hex_data}')
+            
             for i in range(0, len(hex_data), 4):
                 if addr >= END_ADDR:
                     print(f'Reached end address: {addr:08X}')
                     return
-                
-                data_str = ''.join(hex_data[i:i+4])
-                                        
-                if len(data_str) < 8:
-                    data_str = data_str.ljust(8, '0')
-
-                try:
-                    data_32bit = int(data_str, 16)
-                    print(f'Programming addr {addr:08X} with data {data_32bit:08X}') 
-                    program_flash(bus, [(addr, data_32bit)])
+                if i + 3 >= len(hex_data):
+                    break
+                try:                
+                    data_bytes = bytes(int(hex_data[j], 16) for j in range(i, i+4))
                     
+                    if len(data_bytes) != 4:
+                        print(f'Invalid data length: {len(data_bytes)} for data: {data_bytes.hex().upper()}')
+                        continue
+
+                    print(f'Programming addr {addr:08X} with data {data_bytes.hex().upper()}')                
+                    
+                    program_flash(bus, addr, data_bytes)
+
                     while not read_busy_bit(bus, 0x00200020):
-                        print('Checking if busy...')                
-
-                    addr += addr_increment
-
+                        print('check if busy...')
+                    addr += 4 
                 except Exception as e:
                     print(f"Error: {e} for data: {hex_data[i:i+4]}")
 
@@ -119,7 +131,7 @@ def main():
         write_data(bus, 0x00200024, 0xf9ffffff)   
         
         # 7. program flash
-        program_flash_from_file(bus, 'Single_cfm1.txt', 0x00008000, 0x00026fff) 
+        program_flash_from_file(bus, 'singlebig_cfm1_auto.txt', 0x00008000, 0x0002afff) 
  
 
         # 8. re-protect
@@ -131,7 +143,7 @@ def main():
         # 1. un-protect sector 5
         write_data(bus, 0x00200024, 0xf7ffffff)     
         
-        # 2.  erase Sector3
+        # 2.  erase Sector5
         erase_sector(bus, 0x00200024, 0xf7dfffff)        
         
         # 3. check busy bit
@@ -139,17 +151,17 @@ def main():
             print('Still busy...')             
          
         # 4. write erase sector bits to default
-        write_data(bus, 0x00200024, 0xf9ffffff)   
+        write_data(bus, 0x00200024, 0xf7ffffff)   
         
         # 5. program flash
-        program_flash_from_file(bus, 'Single_cfm0.txt', 0x0002b000, 0x00049fff) 
+        program_flash_from_file(bus, 'singlebig_cfm0_auto.txt', 0x0002b000, 0x0004dfff) 
  
 
         # 8. re-protect
         write_data(bus, 0x00200024, 0xffffffff)      
 
-        ######################################Image 1 flow######################################
-        # re-configure
+        #####################################Image 1 flow######################################
+        # # re-configure
         # write_data(bus, 0x00200004, 0x00000001)
         # time.sleep(0.1)
 
